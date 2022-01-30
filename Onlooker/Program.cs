032e92871632c6fc6ea04ca2ca4f725c09585f4d
Zander Baker <zanderbaker100@gmail.com>
2022-01-29 16:48:59 -0800

    Add dev/debug options

--- Onlooker/Program.cs
+++ Onlooker/Program.cs
@@ -1,14 +1,45 @@
-﻿using Onlooker.Monogame;
+﻿using System.Diagnostics;
+using Onlooker.Monogame;
+using Onlooker.Monogame.Logging;
 
 namespace Onlooker;
 
 public static class Program
 {
+    public const string Name = "Onlooker";
+    
     [STAThread]
     public static void Main()
     {
-        using var game = new GameManager("Onlooker");
+        #if DEBUG
+        Console.WriteLine("Open config logs?");
+
+        switch (Console.ReadLine()?.ToLower())
+        {
+            case "yes":
+            case "ye":
+            case "y":
+            case "ok":
+                var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                ExecuteCommandLine($@"explorer ""{appData}\{Name}\logs\{AppLogger.ConfigLog}""");
+                break;
+        }
+        #endif
+        
+        using var game = new GameManager(Name);
         
         game.Run();
+    }
+    
+    private static void ExecuteCommandLine(string command)
+    {
+
+        var processInfo = new ProcessStartInfo("cmd.exe", "/K " + command)
+        {
+            CreateNoWindow = true,
+            UseShellExecute = false
+        };
+
+        Process.Start(processInfo);
     }
 }
