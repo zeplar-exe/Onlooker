3f52d74714657b119d05d221ccdb1f2fdee2e593
Zander Baker <zanderbaker100@gmail.com>
2022-03-05 18:56:06 -0800

    Remove items not added in previous commit

--- Onlooker/IntermediateConfiguration/GUI/GuiConfigGroup.cs
+++
@@ -1,8 +0,0 @@
-using Onlooker.IntermediateConfiguration.GUI.Processing;
-
-namespace Onlooker.IntermediateConfiguration.GUI;
-
-public class GuiConfigGroup : ConfigGroup
-{
-    [RelativeConfigLocation("main_menu.interface")] public GuiDocument MainMenu { get; set; }
-}
